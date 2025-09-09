6b5e482d5dae25e37483d4fde83d854e9aef8669
mrfatihrabbani <mr.fatihrabbani@gmail.com>
2025-09-09 11:58:52 +0700

    CS1-Finished

---
+++ Modul 1/CS_Senin_ES_MochammadRaflyFatihRabbani_2406369021_OOP1/src/Customer.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Customer {
+    String name;
+    Vehicle vehicle;
+
+
+    public Customer(String name, Vehicle vehicle){
+        Scanner details = new Scanner(System.in);
+
+        name =
+        System.out.println("Customer Name : ");
+
+        return;
+    }
+
+
+    public void showDetail(){
+
+    }
+}
---
+++ Modul 1/CS_Senin_ES_MochammadRaflyFatihRabbani_2406369021_OOP1/src/Vehicle.java
@@ -0,0 +1,21 @@
+public class Vehicle {
+    private String brand;
+    private int year;
+    private float price;
+
+    public Vehicle(String brand, int year, float price){
+        this.brand = brand;
+        this.year = year;
+        this.price = price;
+
+    }
+
+    public void showDetail() {
+        System.out.println("Customer Name : ");
+        System.out.println("Brand : " + brand);
+        System.out.println("Year : " + year);
+        System.out.println("Type : " + );
+        System.out.println("Harga : " + price);
+    }
+
+}
